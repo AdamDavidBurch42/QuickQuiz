84c50863f8f46c0aa045921ab8db107b71f3a766
Adam Burch <rewop360@gmail.com>
2021-06-02 09:36:21 -0700

    final commit

--- logic.js
+++ logic.js
@@ -1,1 +1,15 @@
-console.log("hey!");
+let quizArea = document.getElementById('btnGoHere');
+
+console.log(quizArea)
+
+let startButton = document.getElementById('btnStart');
+
+let btns = [1,2,3,4]; 
+
+function getStarted() {
+    for (var i = 0; i < btns.length; i++){
+        btn = document.createElement('BUTTON');
+        btn.innerText = btns[i];
+        quizArea.appendChild(btn);
+    }
+}
